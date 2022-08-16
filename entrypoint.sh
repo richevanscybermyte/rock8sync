05a9f6314a7bf49bafa6f6196d89ac5c5dcfdc34
richevanscybermyte <60758122+richevanscybermyte@users.noreply.github.com>
2022-08-16 17:53:27 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=baseos --newest-only --download-metadata
+/bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=appstream --newest-only --download-metadata
