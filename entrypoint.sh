45926f6459d549bda07275abf113b7a445390cb0
richevanscybermyte <60758122+richevanscybermyte@users.noreply.github.com>
2023-02-17 11:23:58 -0500

    Added EPEL to reposync

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=baseos --newest-only --download-metadata
 /bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=appstream --newest-only --download-metadata
+/bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=epel --newest-only --download-metadata
